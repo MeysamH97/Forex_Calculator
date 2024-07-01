d08f760daee77ca200c86527ea6c7c44873c5a47
MeysamH97 <meysamhasanifx@gmail.com>
2024-07-01 21:36:36 +0430

    create base

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -12,6 +12,16 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   MyTheme? theme;
+  String? marketSelect;
+  var unit;
+
+  var lot = 0.0;
+  var risk = 0.0;
+
+  TextEditingController balanceController = TextEditingController();
+  TextEditingController stopLossController = TextEditingController();
+  TextEditingController riskController = TextEditingController();
+  TextEditingController marketPriceController = TextEditingController();
 
   @override
   void initState() {
@@ -24,7 +34,6 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     var width = MediaQuery.sizeOf(context).width;
     var height = MediaQuery.sizeOf(context).height;
-    var marketSelect;
 
     return SafeArea(
       child: Scaffold(
@@ -40,50 +49,50 @@ class _MainScreenState extends State<MainScreen> {
               theme!.color!.withOpacity(0.0),
             ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
           ),
-          child: Column(
-            children: [
-              Stack(
-                children: [
-                  SizedBox(
-                    width: width,
-                    height: 75,
-                    child: Image.asset(
-                      theme!.headerImage!,
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  Container(
-                    height: 75,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [theme!.color!, Colors.transparent],
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    SizedBox(
+                      width: width,
+                      height: 75,
+                      child: Image.asset(
+                        theme!.headerImage!,
+                        fit: BoxFit.fill,
                       ),
                     ),
-                  ),
-                  Positioned(
-                      left: 20,
-                      top: 10,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          backgroundColor: theme!.color!.withOpacity(0.25),
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.color_lens,
-                              color: theme!.color!,
-                            ),
-                            onPressed: () {
-                              changeTheme();
-                            },
-                          ),
+                    Container(
+                      height: 75,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [theme!.color!, Colors.transparent],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
                         ),
-                      )),
-                ],
-              ),
-              SingleChildScrollView(
-                child: Column(
+                      ),
+                    ),
+                    Positioned(
+                        left: 20,
+                        top: 10,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: CircleAvatar(
+                            backgroundColor: theme!.color!.withOpacity(0.25),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.color_lens,
+                                color: theme!.color!,
+                              ),
+                              onPressed: () {
+                                changeTheme();
+                              },
+                            ),
+                          ),
+                        )),
+                  ],
+                ),
+                Column(
                   children: [
                     Padding(
                       padding: const EdgeInsets.all(20),
@@ -110,13 +119,13 @@ class _MainScreenState extends State<MainScreen> {
                                     'EURO/USD',
                                     'Market 3',
                                     'Market 4',
-                                    'Market 4',
-                                    'Market 4',
-                                    'Market 4',
-                                    'Market 4',
+                                    'Market 5',
                                   ],
                                   onItemSelected: (String value) {
-                                    marketSelect = value;
+                                    setState(() {
+                                      marketSelect = value;
+                                      print(marketSelect);
+                                    });
                                   },
                                 ),
                               ),
@@ -124,6 +133,34 @@ class _MainScreenState extends State<MainScreen> {
                           ),
                           const SizedBox(
                             height: 20,
+                          ),
+                          Visibility(
+                            visible: marketSelect == 'XAU/USD',
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    const Text(
+                                      'Market Price',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                          color: Colors.white),
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    NumberTextField(
+                                        textController: marketPriceController,
+                                        width: width / 3,
+                                        color: theme!.color!),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                              ],
+                            ),
                           ),
                           Row(children: [
                             const Text(
@@ -137,7 +174,9 @@ class _MainScreenState extends State<MainScreen> {
                               width: 10,
                             ),
                             NumberTextField(
-                                width: width / 3, color: theme!.color!),
+                                textController: balanceController,
+                                width: width / 3,
+                                color: theme!.color!),
                             const SizedBox(
                               width: 20,
                             ),
@@ -160,18 +199,121 @@ class _MainScreenState extends State<MainScreen> {
                                   'Rial ',
                                 ],
                                 onItemSelected: (String value) {
-                                  marketSelect = value;
+                                  unit = value;
                                 },
                               ),
                             ),
                           ]),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(children: [
+                            const Text(
+                              'Stop Loss (Pips)',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.white),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            NumberTextField(
+                                textController: stopLossController,
+                                width: width / 4,
+                                color: theme!.color!),
+                          ]),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(children: [
+                            const Text(
+                              'Risk ( \% )',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.white),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            NumberTextField(
+                                textController: riskController,
+                                width: width / 4,
+                                color: theme!.color!),
+                          ]),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  Calculate();
+                                },
+                                child: Container(
+                                  width: width / 2,
+                                  padding: EdgeInsets.all(15),
+                                  alignment: AlignmentDirectional.center,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: theme!.color,
+                                  ),
+                                  child: Text(
+                                    'Calculate Lot',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                        color: Colors.white),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Divider(),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Visibility(
+                            visible: risk != 0,
+                            child: Row(
+                              children: [
+                                Text(
+                                  'Amount of Risk : ${risk.toStringAsFixed(2)} $unit',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                      color: Colors.red[800]),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Visibility(
+                            visible: lot != 0,
+                            child: Row(
+                              children: [
+                                Text('Lot Volume : ${lot.toStringAsFixed(2)} ',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: theme!.color!),),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
                     )
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -190,5 +332,24 @@ class _MainScreenState extends State<MainScreen> {
     });
   }
 
+  String Calculate() {
+    double? balance = double.tryParse(balanceController.text);
+    double? riskPercentage = double.tryParse(riskController.text);
+    double? stopLossPoints = double.tryParse(stopLossController.text);
+    double pointValue = double.tryParse(marketPriceController.text) != null
+        ? double.tryParse(marketPriceController.text)! / 1000
+        : 1; // برای مثال مقدار ارزش هر پوینت برای EUR/USD
 
+    if (balance != null && riskPercentage != null && stopLossPoints != null) {
+      double riskAmount = balance * (riskPercentage / 100);
+      risk = riskAmount;
+      double lotSize = riskAmount / (stopLossPoints * pointValue);
+      lot = lotSize;
+      setState(() {});
+      return '';
+    } else {
+      setState(() {});
+      return "Invalid input for balance, risk percentage or stop loss points";
+    }
+  }
 }
--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -4,26 +4,27 @@ import 'package:intl/intl.dart';
 class NumberTextField extends StatefulWidget {
   final Color color;
   final double width;
+  final TextEditingController textController;
 
-  NumberTextField({required this.color, required this.width});
+  NumberTextField({required this.color, required this.width, required this.textController});
 
   @override
   _NumberTextFieldState createState() => _NumberTextFieldState();
 }
 
 class _NumberTextFieldState extends State<NumberTextField> {
-  final TextEditingController _controller = TextEditingController();
+
   final NumberFormat _formatter = NumberFormat('#,###');
 
   @override
   void initState() {
     super.initState();
-    _controller.addListener(() {
-      final text = _controller.text.replaceAll(',', '');
+    widget.textController.addListener(() {
+      final text = widget.textController.text.replaceAll(',', '');
       try {
         final int number = int.parse(text.isEmpty ? '0' : text);
         final formattedText = _formatter.format(number);
-        _controller.value = _controller.value.copyWith(
+        widget.textController.value = widget.textController.value.copyWith(
           text: formattedText,
           selection: TextSelection.collapsed(offset: formattedText.length),
         );
@@ -40,7 +41,7 @@ class _NumberTextFieldState extends State<NumberTextField> {
       height: 50,
       decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10),),
       child: TextField(
-        controller: _controller,
+        controller: widget.textController,
         keyboardType: TextInputType.number,
         inputFormatters: [FilteringTextInputFormatter.digitsOnly],
         style: TextStyle(fontSize: 14),
@@ -54,11 +55,5 @@ class _NumberTextFieldState extends State<NumberTextField> {
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 }
